992431a22f878362d1c33cf332728967bbce363c
Nikita Zhavoronkov <nikzh@nikzh.com>
2023-06-13 13:37:29 -0400

    Add support for .avax handles

--- Modules/AvalancheMainModule.php
+++ Modules/AvalancheMainModule.php
@@ -27,5 +27,51 @@ final class AvalancheMainModule extends EVMMainModule implements Module
         {
             return '0';
         };
+
+        // Handles
+        $this->handles_implemented = true;
+        $this->handles_regex = '/(.*)\.avax/';
+        $this->api_get_handle = function($handle)
+        {
+            /*  Okay, here things go a bit ugly. Sorry about that. Since there's no good PHP library for Avvy Domains, we
+             *  have to use a Python library. The recommended way is to set up this: https://github.com/avvydomains/python-client
+             *  and set up a proxy for it. Here's how can the Python part (`resolve.py`) look like:
+             *
+             *  from avvy import AvvyClient
+             *  from web3 import Web3
+             *  import sys
+             *  w3 = Web3(Web3.HTTPProvider('...'))
+             *  avvy = AvvyClient(w3)
+             *  evm_address = avvy.name(sys.argv[1]).resolve(avvy.RECORDS.EVM)
+             *  print(f'{evm_address}')
+             *
+             *  And the PHP part (`index.php`):
+             *
+             *  <?php if (!isset($_GET['name'])) return;
+             *  $resolves_to = trim(`python3 resolve.py {$_GET['name']}`);
+             *  if (!$resolves_to || $resolves_to === 'None') return;
+             *  echo '{"result": "' . strtolower($resolves_to) . '"}';
+             *
+             *  Then, simply point an nginx proxy to `index.php`, and add this proxy to `HANDLE_NODES`
+             *
+             *  Once there's a better PHP support or some on-chain contracts to resolve names, we'll switch to this.
+             */
+
+            if (!preg_match($this->handles_regex, $handle))
+                return null;
+
+            $handle_nodes = envm($this->module, 'HANDLE_NODES');
+            $handle_node = $handle_nodes[array_rand($handle_nodes)];
+
+            try
+            {
+                $address = requester_single(daemon: $handle_node, endpoint: '?name=' . $handle, result_in: 'result');
+                return $address;
+            }
+            catch (RequesterEmptyResponseException)
+            {
+                return null;
+            }
+        };
     }
 }
