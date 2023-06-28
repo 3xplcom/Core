635a8232c9f8e6c329474a291254049729d7dc9d
Nikita Zhavoronkov <nikzh@nikzh.com>
2023-06-27 22:51:50 -0400

    Fix missing Solana blocks (2) `Block not available for slot` turned out to be temporary as well

--- Modules/Common/SolanaLikeMinimalModule.php
+++ Modules/Common/SolanaLikeMinimalModule.php
@@ -80,8 +80,7 @@ abstract class SolanaLikeMinimalModule extends CoreModule
         }
         catch (RequesterException $e)
         {
-            if (strstr($e->getMessage(), 'was skipped, or missing due to ledger jump to recent snapshot')
-                || strstr($e->getMessage(), 'Block not available for slot')) // Empty slot
+            if (strstr($e->getMessage(), 'was skipped, or missing due to ledger jump to recent snapshot')) // Empty slot
             {
                 $this->block_time = date('Y-m-d H:i:s', 0);
                 $this->set_return_events([]);
