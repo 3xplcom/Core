9bdbefdb2209fbc84368629b2436879cabbebeff
Nikita Zhavoronkov <nikzh@nikzh.com>
2023-08-01 14:08:50 -0400

    Add a note about Nethermind

--- Modules/GnosisChainMainModule.php
+++ Modules/GnosisChainMainModule.php
@@ -4,7 +4,10 @@
  *  Copyright (c) 2023 3xpl developers, 3@3xpl.com
  *  Distributed under the MIT software license, see the accompanying file LICENSE.md  */
 
-/*  This is the main Gnosis Chain module. It requires either a Nethermind or an Erigon node to run (but the latter is much faster).  */
+/*  This is the main Gnosis Chain module. It requires either a Nethermind or an Erigon node to run (but the latter is much faster).
+ *  Not that for some transactions such as 0x0cd5fc7f3d1ff461a01de546c8c32d4ec74fb66e640dbe4ed537f3a6ab2e27bc, Nethermind returns
+ *  the created contract address neither in the transaction info, nor in the transaction receipt. That's probably a bug with
+ *  Nethermind which is not observed with Erigon. `No address` exception will be thrown for such transactions.  */
 
 final class GnosisChainMainModule extends EVMMainModule implements Module
 {
