301df7527ee12d50e95efd4ae804796bc51eade4
Nikita Zhavoronkov <nikzh@nikzh.com>
2023-10-05 20:27:02 -0400

    Add Botanix support

---
+++ Modules/BotanixMainModule.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+/*  Idea (c) 2023 Nikita Zhavoronkov, nikzh@nikzh.com
+ *  Copyright (c) 2023 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
+ *  Distributed under the MIT software license, see LICENSE.md  */
+
+/*  This is the main Botanix module. It requires either a geth node to run.  */
+
+final class BotanixMainModule extends EVMMainModule implements Module
+{
+    function initialize()
+    {
+        // CoreModule
+        $this->blockchain = 'botanix';
+        $this->module = 'botanix-main';
+        $this->is_main = true;
+        $this->first_block_date = '2023-10-05';
+        $this->currency = 'botanix-bitcoin';
+        $this->currency_details = ['name' => 'Bitcoin', 'symbol' => 'BTC', 'decimals' => 18, 'description' => null];
+
+        // EVMMainModule
+        $this->evm_implementation = EVMImplementation::geth;
+        $this->extra_features = [];
+        $this->reward_function = function($block_id)
+        {
+            return '0';
+        };
+    }
+}
---
+++ Modules/BotanixTraceModule.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+/*  Idea (c) 2023 Nikita Zhavoronkov, nikzh@nikzh.com
+ *  Copyright (c) 2023 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
+ *  Distributed under the MIT software license, see LICENSE.md  */
+
+/*  This module processes internal Botanix transactions (using block tracing). It requires an archival geth node to run.  */
+
+final class BotanixTraceModule extends EVMTraceModule implements Module
+{
+    function initialize()
+    {
+        // CoreModule
+        $this->blockchain = 'botanix';
+        $this->module = 'botanix-trace';
+        $this->complements = 'botanix-main';
+        $this->is_main = false;
+        $this->first_block_date = '2023-10-05';
+
+        // EVMTraceModule
+        $this->evm_implementation = EVMImplementation::geth;
+    }
+}
