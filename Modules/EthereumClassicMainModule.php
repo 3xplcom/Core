4ccc7072f40f505acd1090ed236c2e74e4528335
Nikita Zhavoronkov <nikzh@nikzh.com>
2023-12-19 21:29:57 -0400

    Add Ethereum Classic support

---
+++ Modules/EthereumClassicMainModule.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types = 1);
+
+/*  Idea (c) 2023 Nikita Zhavoronkov, nikzh@nikzh.com
+ *  Copyright (c) 2023 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
+ *  Distributed under the MIT software license, see LICENSE.md  */
+
+/*  This is the main Ethereum Classic module. It requires either a geth node to run. Please note that `status` is
+ *  not available on geth for some older transactions when requesting receipts and a special fix is required.  */
+
+final class EthereumClassicMainModule extends EVMMainModule implements Module
+{
+    function initialize()
+    {
+        // CoreModule
+        $this->blockchain = 'ethereum-classic';
+        $this->module = 'ethereum-classic-main';
+        $this->is_main = true;
+        $this->first_block_date = '2015-07-30';
+        $this->currency = 'ethereum-classic';
+        $this->currency_details = ['name' => 'Ethereum Classic', 'symbol' => 'ETC', 'decimals' => 18, 'description' => null];
+
+        // EVMMainModule
+        $this->evm_implementation = EVMImplementation::geth;
+        $this->extra_features = [EVMSpecialFeatures::HasOrHadUncles];
+        $this->reward_function = function($block_id)
+        {
+            // https://github.com/ethereumproject/ECIPs/blob/master/ECIPs/ECIP-1017.md
+            // https://ethereumclassic.org/why-classic/sound-money#known-future-supply
+
+            $base_reward = '5000000000000000000';
+            $reductions = intdiv($block_id - 1, 5_000_000);
+
+            for ($i = 0; $i < $reductions; $i++)
+                $base_reward = bcmul($base_reward, '0.8');
+
+            return $base_reward;
+        };
+    }
+}
