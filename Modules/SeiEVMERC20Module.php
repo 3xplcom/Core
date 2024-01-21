9ab6aafdd09ec5b488bfb2d29b453412c6901f64
Nikita Zhavoronkov <nikzh@nikzh.com>
2024-01-20 21:55:09 -0400

    Rename the blockchain into `sei-evm`

--- Modules/SeiEVMERC20Module.php
+++ Modules/SeiEVMERC20Module.php
@@ -1,20 +1,20 @@
 <?php declare(strict_types = 1);
 
 /*  Idea (c) 2023 Nikita Zhavoronkov, nikzh@nikzh.com
- *  Copyright (c) 2023 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
+ *  Copyright (c) 2023-2024 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
  *  Distributed under the MIT software license, see LICENSE.md  */
 
-/*  This module processes ERC-20 token transfers in EVM Sei.  */
+/*  This module processes ERC-20 token transfers in Sei EVM.  */
 
 final class SeiEVMERC20Module extends EVMERC20Module implements Module
 {
     function initialize()
     {
         // CoreModule
-        $this->blockchain = 'sei';
+        $this->blockchain = 'sei-evm';
         $this->module = 'sei-evm-erc-20';
         $this->is_main = false;
-        $this->first_block_date = '2023-12-26'; // TODO: this is block_date for Sei devnet
+        $this->first_block_date = '2023-12-26'; // This is for the devnet
         $this->first_block_id = 0;
     }
 }
--- Modules/SeiEVMMainModule.php
+++ Modules/SeiEVMMainModule.php
@@ -1,22 +1,22 @@
 <?php declare(strict_types = 1);
 
 /*  Idea (c) 2023 Nikita Zhavoronkov, nikzh@nikzh.com
- *  Copyright (c) 2023 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
+ *  Copyright (c) 2023-2024 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
  *  Distributed under the MIT software license, see LICENSE.md  */
 
-/*  This is the EVM main Sei module. */
+/*  This is the main Sei EVM module. */
 
 final class SeiEVMMainModule extends EVMMainModule implements Module
 {
     function initialize()
     {
         // CoreModule
-        $this->blockchain = 'sei';
+        $this->blockchain = 'sei-evm';
         $this->module = 'sei-evm-main';
         $this->is_main = true;
-        $this->first_block_date = '2023-12-26'; // TODO: this is block_date for Sei devnet
+        $this->first_block_date = '2023-12-26'; // This is for the devnet
         $this->first_block_id = 0;
-        $this->currency = 'sei';
+        $this->currency = 'sei-evm';
         $this->currency_details = ['name' => 'Sei', 'symbol' => 'SEI', 'decimals' => 18, 'description' => null];
 
         // EVMMainModule
