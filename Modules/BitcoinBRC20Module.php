ad4b27d472336374994b0179a19a42af68b8b3bf
Nikita Zhavoronkov <nikzh@nikzh.com>
2024-02-06 20:22:54 -0400

    Code formatting and fixes

--- Modules/BitcoinBRC20Module.php
+++ Modules/BitcoinBRC20Module.php
@@ -1,11 +1,12 @@
-<?php
+<?php declare(strict_types=1);
 
 /*  Idea (c) 2023 Nikita Zhavoronkov, nikzh@nikzh.com
  *  Copyright (c) 2023-2024 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
  *  Distributed under the MIT software license, see LICENSE.md  */
 
-/*  This module processes BRC-20 tokens on Bitcoin.
-*   It requires https://github.com/hirosystems/ordinals-api */
+/*  This module processes BRC-20 token transfers on Bitcoin.
+ *  It requires https://github.com/hirosystems/ordinals-api and a Bitcoin Core node to run.  */
+
 final class BitcoinBRC20Module extends OrdinalsBRC20Module implements Module
 {
     function initialize()
@@ -14,7 +15,7 @@ final class BitcoinBRC20Module extends OrdinalsBRC20Module implements Module
         $this->blockchain = 'bitcoin';
         $this->module = 'bitcoin-brc-20';
         $this->is_main = false;
-        $this->first_block_id = 700_000;
+        $this->first_block_id = 779_832;
         $this->first_block_date = '2023-03-08';
     }
 }
--- Modules/Common/OrdinalsBRC20Module.php
+++ Modules/Common/OrdinalsBRC20Module.php
@@ -4,16 +4,17 @@
  *  Copyright (c) 2023-2024 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
  *  Distributed under the MIT software license, see LICENSE.md  */
 
-/*  This module processes BRC-20 tokens on Bitcoin.
-*   It requires https://github.com/hirosystems/ordinals-api */
+/*  This module processes BRC-20 token transfers on Bitcoin-like blockchains.
+ *  It requires https://github.com/hirosystems/ordinals-api and a Core node to run.  */
 
 abstract class OrdinalsBRC20Module extends CoreModule
 {
     public ?array $events_table_fields = ['block', 'transaction', 'sort_key', 'time', 'currency', 'address', 'effect', 'extra'];
     public ?array $events_table_nullable_fields = ['extra'];
-    public ?array $currencies_table_fields = ['id', 'name', 'description', 'decimals'];
+    public ?array $currencies_table_fields = ['id', 'description', 'decimals'];
     public ?array $currencies_table_nullable_fields = [];
-    public ?ExtraDataModel $extra_data_model = ExtraDataModel::Type;
+    public ?ExtraDataModel $extra_data_model = ExtraDataModel::Type; // TODO: $extra_data_details
+
     public ?bool $should_return_events = true;
     public ?bool $should_return_currencies = true;
     public ?bool $allow_empty_return_events = true;
@@ -51,35 +52,48 @@ abstract class OrdinalsBRC20Module extends CoreModule
     final public function ensure_block($block_id, $break_on_first = false)
     {
         $this->btc_nodes = envm($this->module, "BITCOIN_RPC");
-        if (count($this->btc_nodes) === 1) {
+
+        if (count($this->btc_nodes) === 1)
+        {
             $this->block_hash = requester_single($this->btc_nodes[0],
                 params: ['method' => 'getblockhash', 'params' => [(int)$block_id]],
                 result_in: 'result', timeout: $this->timeout);
-        } else {
+        }
+        else
+        {
             $multi_curl = [];
 
-            foreach ($this->btc_nodes as $node) {
+            foreach ($this->btc_nodes as $node)
+            {
                 $multi_curl[] = requester_multi_prepare($node, params: ['method' => 'getblockhash', 'params' => [(int)$block_id]],
                     timeout: $this->timeout);
                 if ($break_on_first) break;
             }
-            try {
+
+            try
+            {
                 $curl_results = requester_multi($multi_curl, limit: count($this->nodes), timeout: $this->timeout);
-            } catch (RequesterException $e) {
+            }
+            catch (RequesterException $e)
+            {
                 throw new RequesterException("ensure_block(block_id: {$block_id}): no connection, previously: " . $e->getMessage());
             }
 
             $hash = requester_multi_process($curl_results[0], result_in: 'result');
-            if (count($curl_results) > 1) {
-                foreach ($curl_results as $result) {
-                    if (requester_multi_process($result, result_in: 'result') !== $hash) {
+
+            if (count($curl_results) > 1)
+            {
+                foreach ($curl_results as $result)
+                {
+                    if (requester_multi_process($result, result_in: 'result') !== $hash)
+                    {
                         throw new ConsensusException("ensure_block(block_id: {$block_id}): no consensus");
                     }
                 }
             }
+
             $this->block_hash = $hash;
         }
-
     }
 
     final public function post_post_initialize()
@@ -89,11 +103,11 @@ abstract class OrdinalsBRC20Module extends CoreModule
 
     final public function pre_process_block($block_id)
     {
-
         $block_hash = $this->block_hash;
         $block = requester_single($this->btc_nodes[0], endpoint: "/rest/block/notxdetails/{$block_hash}.json", timeout: $this->timeout);
         $true_block_time = date('Y-m-d H:i:s', (int)$block['time']);
         $sorted_tx = array_flip($block['tx']);
+
         $block = requester_single($this->select_node(),
             endpoint: "/ordinals/v1/brc-20/activity?block_height={$block_id}&limit={$this->limit}",
             timeout: $this->timeout);
@@ -101,6 +115,7 @@ abstract class OrdinalsBRC20Module extends CoreModule
         if (count($block['results']) != 0)
         {
             $this->block_time = to_timestamp_from_long_unixtime($block['results'][0]['timestamp']);
+
             if ($true_block_time !== $this->block_time)
                 throw new ModuleError("Timestamp of btc node and ord indexer doesn't match on block {$this->block_id}");
         }
@@ -136,21 +151,15 @@ abstract class OrdinalsBRC20Module extends CoreModule
         }
 
         // sort according to tx index in the block
-        usort($curl_results, function ($a, $b)
-        {
-            return [$a['tx_index'],
-                ]
-                <=>
-                [$b['tx_index'],
-                ];
-        });
+        usort($curl_results, function ($a, $b) { return [$a['tx_index']] <=> [$b['tx_index']]; });
 
         $events = $currencies_to_process = [];
         $sort_key = 0;
 
         foreach ($curl_results as $op)
         {
-            switch ($op['operation']) {
+            switch ($op['operation'])
+            {
                 case 'deploy':
                     $events[] = [
                         'transaction' => $op['tx_id'],
@@ -168,7 +177,9 @@ abstract class OrdinalsBRC20Module extends CoreModule
                         'effect' => '0',
                         'extra' => $op['operation'],
                     ];
+                    $currencies_to_process[] = $op['ticker'];
                     break;
+
                 case 'mint':
                     $events[] = [
                         'transaction' => $op['tx_id'],
@@ -186,7 +197,9 @@ abstract class OrdinalsBRC20Module extends CoreModule
                         'effect' => str_replace('.', '', $op['mint']['amount']),
                         'extra' => $op['operation'],
                     ];
+                    $currencies_to_process[] = $op['ticker'];
                     break;
+
                 case 'transfer_send':
                     $events[] = [
                         'transaction' => $op['tx_id'],
@@ -204,11 +217,12 @@ abstract class OrdinalsBRC20Module extends CoreModule
                         'effect' => str_replace('.', '', $op['transfer_send']['amount']),
                         'extra' => null,
                     ];
+                    $currencies_to_process[] = $op['ticker'];
                     break;
+
                 default:
                     break;
             }
-            $currencies_to_process[] = $op['ticker'];
         }
 
         $currencies = [];
@@ -218,6 +232,7 @@ abstract class OrdinalsBRC20Module extends CoreModule
         if ($currencies_to_process)
         {
             $multi_curl = [];
+
             foreach ($currencies_to_process as $currency)
             {
                 $currency = rawurlencode($currency);
@@ -234,7 +249,6 @@ abstract class OrdinalsBRC20Module extends CoreModule
             {
                 $currencies[] = [
                     'id' => $currency['token']['ticker'],
-                    'name' => $currency['token']['number'],  // not sure about this key @Har01d
                     'description' => $currency['token']['id'],
                     'decimals' => $currency['token']['decimals'] ?? 18
                 ];
@@ -257,11 +271,13 @@ abstract class OrdinalsBRC20Module extends CoreModule
             return [];
 
         $real_currencies = [];
+
         foreach ($currencies as $c)
             $real_currencies[] = rawurlencode(explode('/', $c)[1]);
 
         $data = [];
         $query_params = "ticker=" . implode("&ticker=",$real_currencies);
+
         $first_query = requester_single($this->select_node(),
             endpoint: "/ordinals/v1/brc-20/balances/{$address}?{$query_params}&limit={$this->limit}",
             timeout: $this->timeout);
