dca300f217938cad40826b878a053c9d92acafa0
Nikita Zhavoronkov <nikzh@nikzh.com>
2024-02-27 12:25:55 -0400

    Small fixes

--- Modules/Common/StellarLikeMainModule.php
+++ Modules/Common/StellarLikeMainModule.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types = 1);
 
 /*  Idea (c) 2023 Nikita Zhavoronkov, nikzh@nikzh.com
- *  Copyright (c) 2023 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
+ *  Copyright (c) 2023-2024 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
  *  Distributed under the MIT software license, see LICENSE.md  */
 
 /*  This module processes main transactions in Stellar. Requires a Stellar node.  */
@@ -62,41 +62,50 @@ abstract class StellarLikeMainModule extends CoreModule
         $paging_token = $this->paging_token;
         $paging_token = bcadd($paging_token, $diff_200); // for escaping a lot of ifs 
         $tx_path = "ledgers/{$block_id}/transactions?order=asc&limit=%s&include_failed=true&cursor=%s";
+
         for ($i = $this->transaction_count; $i > 0;) 
         {
             $limit = 200;
+
             if ($limit < $i) 
             {
                 $i -= $limit;
                 $paging_token = bcsub($paging_token, $diff_200);
-            } else {
+            }
+            else
+            {
                 $limit = $i;
                 $i = 0;
                 $diff_limit = bcmul((string)$limit, "-4096");
                 $paging_token = bcsub($paging_token, $diff_limit);
             }
+
             $path_formed = sprintf($tx_path, $limit, $paging_token);
+
             $multi_curl[] = requester_multi_prepare(
                 $this->select_node(),
                 endpoint: $path_formed,
                 timeout: $this->timeout
             );
         }
+
         try
         {
             $curl_results = requester_multi($multi_curl, limit: count($this->nodes), timeout: $this->timeout);
         }
         catch (RequesterException $e)
         {
-            throw new RequesterException("ensure_block(block_id: {$$this->block_id}): no connection, previously: " . $e->getMessage());
+            throw new RequesterException("ensure_block(block_id: {$this->block_id}): no connection, previously: " . $e->getMessage());
         }
+
         foreach ($curl_results as $v)
-                $transactions = array_merge($transactions, requester_multi_process($v, ignore_errors: true)['_embedded']['records']);
+            $transactions = array_merge($transactions, requester_multi_process($v, ignore_errors: true)['_embedded']['records']);
 
         // we are not sure that it will happen, but let's keep it here
-        if($this->transaction_count != count($transactions)) 
+        if ($this->transaction_count != count($transactions))
         {
             unset($transactions);
+
             $transactions = $this->get_data_with_cursor(
                 $this->select_node() . "ledgers/{$block_id}/transactions?order=desc&limit=%s&include_failed=true&cursor=%s", 
                 $this->transaction_count);
@@ -104,7 +113,7 @@ abstract class StellarLikeMainModule extends CoreModule
 
         $sort_key = 0;
 
-        foreach($transactions as $tx) 
+        foreach ($transactions as $tx)
         {
             $events[] = [
                 'transaction' => $tx['id'],
@@ -130,7 +139,7 @@ abstract class StellarLikeMainModule extends CoreModule
                 'sort_key' => $sort_key++,
                 'effect' => '-0',
                 'failed' => false,
-                'extra' => 'f',
+                'extra' => 'o', // We need something different here
             ];
 
             $events[] = [
@@ -139,7 +148,7 @@ abstract class StellarLikeMainModule extends CoreModule
                 'sort_key' => $sort_key++,
                 'effect' => '0',
                 'failed' => false,
-                'extra' => 'f',
+                'extra' => 'o',
             ];
         }
 
@@ -159,17 +168,19 @@ abstract class StellarLikeMainModule extends CoreModule
     // Getting balances from the node
     public function api_get_balance($address)
     {
-        $account_balances =requester_single($this->select_node(),
+        $account_balances = requester_single($this->select_node(),
             endpoint: "accounts/{$address}",
             result_in: 'balances',
             timeout: $this->timeout);
-        foreach($account_balances as $balance)
+
+        foreach ($account_balances as $balance)
         {
-            if($balance['asset_type'] === 'native')
+            if ($balance['asset_type'] === 'native')
             {
                 return $this->to_7($balance['balance']);
             }
         }
-        throw new ModuleError("Unknown code flow");
+
+        throw new ModuleError('Unknown code flow');
     }
 }
