0007fa00d386e37657d3064cf235aa844af43b08
jzethar <info+jzethar@blockchair.com>
2024-03-13 03:12:48 +0600

    ethereum cancun preparation, add blob transaction type

--- Modules/Common/EVMMainModule.php
+++ Modules/Common/EVMMainModule.php
@@ -265,6 +265,13 @@ abstract class EVMMainModule extends CoreModule
 
                 if (in_array(EVMSpecialFeatures::HasSystemTransactions, $this->extra_features))
                     $transaction_data[($general_data[$i]['hash'])]['type'] = $receipt_data[$i]['type'];
+                
+                if (in_array(EVMSpecialFeatures::EIP4844, $this->extra_features))
+                {
+                    $transaction_data[($general_data[$i]['hash'])]['type'] = $receipt_data[$i]['type'];
+                    $transaction_data[($general_data[$i]['hash'])]['blobGasPrice'] = $receipt_data[$i]['blobGasPrice'] ?? null;
+                    $transaction_data[($general_data[$i]['hash'])]['blobGasUsed'] = $receipt_data[$i]['blobGasUsed'] ?? null;
+                }
             }
         }
         else // Mempool processing
@@ -404,6 +411,14 @@ abstract class EVMMainModule extends CoreModule
                     if ($transaction['type'] === '0x7e')
                     {
                         $this_burned = $this_to_miner = '0';
+                    }
+                }
+                if (in_array(EVMSpecialFeatures::EIP4844, $this->extra_features))
+                {
+                    if ($transaction['type'] === '0x3')
+                    {
+                        $blob_fee = bcmul(to_int256_from_0xhex($transaction['blobGasUsed']), to_int256_from_0xhex($transaction['blobGasPrice']));
+                        $this_burned = bcadd($this_burned, $blob_fee);
                     }
                 }
             }
