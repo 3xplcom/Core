0c6fe06484cb7f696cc14d21732d4077c5683c36
Nikita Zhavoronkov <nikzh@nikzh.com>
2024-07-22 14:33:21 -0400

    Smol fix

--- Modules/SolanaMainModule.php
+++ Modules/SolanaMainModule.php
@@ -4,7 +4,7 @@
  *  Copyright (c) 2023-2024 3xpl developers, 3@3xpl.com
  *  Distributed under the MIT software license, see the accompanying file LICENSE.md  */
 
-/*  This module processes basic Solana transfers.  */
+/*  This module processes main Solana transfers.  */
 
 final class SolanaMainModule extends SolanaLikeMainModule implements Module, BalanceSpecial
 {
@@ -12,7 +12,7 @@ final class SolanaMainModule extends SolanaLikeMainModule implements Module, Bal
     {
         // General
         $this->blockchain = 'solana';
-        $this->module = 'solana-minimal';
+        $this->module = 'solana-main';
         $this->is_main = true;
 
         // Special
@@ -23,6 +23,7 @@ final class SolanaMainModule extends SolanaLikeMainModule implements Module, Bal
         $this->currency = 'solana';
         $this->currency_details = ['name' => 'Solana', 'symbol' => 'SOL', 'decimals' => 9, 'description' => null];
 
+        // Handles
         $this->handles_implemented = true;
         $this->handles_regex = '/(.*)\.sol/';
         $this->api_get_handle = function ($handle)
