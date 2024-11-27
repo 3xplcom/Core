4e7663330f25c02bcf574cf1dba6532fbd0cf4ad
jzethar <info+jzethar@blockchair.com>
2024-11-27 18:57:56 +0500

    fix copyright

--- Modules/RippleNFTModule.php
+++ Modules/RippleNFTModule.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types = 1);
 
 /*  Idea (c) 2023 Nikita Zhavoronkov, nikzh@nikzh.com
- *  Copyright (c) 2023 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
+ *  Copyright (c) 2023-2024 3xpl developers, 3@3xpl.com, see CONTRIBUTORS.md
  *  Distributed under the MIT software license, see LICENSE.md  */
 
 /*  This module works with NFT in Ripple. Requires a Ripple node.  */
